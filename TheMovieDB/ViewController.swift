a16286b6edd27c01c4326c8ab93ad90051ceebe7
Manuel Alejandro Verdugo <manuel.alejandro.verdugo@gmail.com>
2021-09-14 18:56:00 -0600

    Initial Commit

---
+++ TheMovieDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMovieDB
+//
+//  Created by Manuel Alejandro Verdugo Pérez on 14/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
