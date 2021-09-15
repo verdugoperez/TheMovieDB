845665f0b1193f2ce50666b2d9f753962638c641
Manuel Alejandro Verdugo <manuel.alejandro.verdugo@gmail.com>
2021-09-15 17:36:44 -0600

    Ajustes pantalla de detalle

--- TheMovieDB/Controllers/PeliculaDetalleViewController.swift
+++ TheMovieDB/Controllers/PeliculaDetalleViewController.swift
@@ -10,14 +10,24 @@ import UIKit
 class PeliculaDetalleViewController: UIViewController {
     
     @IBOutlet weak var portadaImageView: UIImageView!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var calificacionLabel: UILabel!
+    @IBOutlet weak var fechaLabel: UILabel!
+    
     var pelicula: Pelicula!
     private let peliculasManager = PeliculasManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        actualizaUI()
+    }
+    
+    private func actualizaUI(){
         self.title = pelicula.title
+        overviewLabel.text = pelicula.overview
+        calificacionLabel.text = "\(pelicula.voteAverage)⭐️"
+        fechaLabel.text = pelicula.releaseDate.convertirAFecha()?.convertirAString()
         obtenerImagenPortada()
     }
     
