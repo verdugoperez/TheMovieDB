a7c6e80c6bf541d495c4071e248757b2f3422c09
Manuel Alejandro Verdugo <manuel.alejandro.verdugo@gmail.com>
2021-09-14 19:30:26 -0600

    Crear modelo de pelicula

---
+++ TheMovieDB/Modelo/Pelicula.swift
@@ -0,0 +1,25 @@
+//
+//  Pelicula.swift
+//  TheMovieDB
+//
+//  Created by Manuel Alejandro Verdugo Pérez on 14/09/21.
+//
+
+import Foundation
+
+struct Pelicula {
+    var adult: Bool
+    var backdropPath: String
+    var genreIds: [Int]
+    var id: Int
+    var originalLanguage: String
+    var originalTitle: String
+    var overview: String
+    var popularity: Float
+    var posterPath: String
+    var releaseDate: String
+    var title: String
+    var video: Bool
+    var voteAverage: Float
+    var voteCount: Int
+}
